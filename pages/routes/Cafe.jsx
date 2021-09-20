99c768a5d4d675ff783b4981464683d93132af83
newcarvalho <new.figueiredo@gmail.com>
2021-09-19 21:00:59 -0300

    correcoes

--- pages/routes/Cafe.jsx
+++ pages/routes/Cafe.jsx
@@ -8,7 +8,7 @@ export default function Cafes() {
             <Cards srcFoto  ='https://revistadeguste.com/wp-content/uploads/2019/02/b4068f2e14767c8bb83226c6655dec238759ffc9f32e4ff6e0660edb368e11f6.jpg' 
                    nomeCard ='Balzac café' 
                    linkCard ='https://www.instagram.com/sigabalzaccafe/'/>
-            <Cards srcFoto  ='https://lh3.googleusercontent.com/proxy/rAn6IVkRA6NUlA8x4t2GEpGB3CR__0tXrumJ8pAvaW1sXFNLv_pCdP8bEvTfg7RA92KDjr-LlW-ZARWAWxPrv7bRzpn22iLoMfh7rVaY' 
+            <Cards srcFoto  ='https://lh3.googleusercontent.com/p/AF1QipP5FTZBzSnDCzBZW9KLJ-TaxNMmgAqB2UH11FXj=w600-k' 
                    nomeCard ='Barões do café' 
                    linkCard ='https://linktr.ee/baroescafe'/>
             <Cards srcFoto  ='https://www.chapelatto.com/____impro/1/onewebmedia/19072019-IMG_7989.JPG?etag=W%2F%221d6815-60d72c81%22&sourceContentType=image%2Fjpeg&ignoreAspectRatio&resize=836%2B557&extract=0%2B0%2B792%2B557&quality=85' 
